73e98b727dd6fd6f0701f7e4ff63acd7d9e5cc99
Niti2102 <nitishm.2102@gmail.com>
2025-07-22 22:13:47 +0530

    guvicorn

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Get the absolute path of the directory where the current file is located
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Set the GROQ_API_KEY environment variable
-os.environ["GROQ_API_KEY"] = "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"
+# Fetch the GROQ_API_KEY from environment
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+# Optional: raise an error if the key is missing
+if not groq_api_key:
+    raise ValueError("GROQ_API_KEY not found in environment variables")
 
 # Define the path for the vector store
-vector_store_path = os.path.join(current_directory, "faiss_index") 
+vector_store_path = os.path.join(current_directory, "faiss_index")
